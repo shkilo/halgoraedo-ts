39f6b083a4530d5a370efa58497d01d508baa5f6
shkillo <kyle.hwan.k@gmail.com>
2021-03-11 20:29:21 +0900

    feat: export project module

---
+++ src/project/project.module.ts
@@ -0,0 +1,14 @@
+import { Module } from '@nestjs/common';
+import { SequelizeModule } from '@nestjs/sequelize';
+import { Project } from './project.model';
+import { Section } from './section.model';
+import { ProjectController } from './project.controller';
+import { ProjectService } from './project.service';
+
+@Module({
+  imports: [SequelizeModule.forFeature([Project, Section])],
+  providers: [ProjectService],
+  controllers: [ProjectController],
+  exports: [ProjectService],
+})
+export class ProjectModule {}
