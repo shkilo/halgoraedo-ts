8eb0950efdc8b01f0bc771b601ffe8b51e89b351
shkillo <kyle.hwan.k@gmail.com>
2021-03-11 20:32:13 +0900

    feat: implement passport jwt strategy

---
+++ src/auth/passport-strategies/jwt.strategy.ts
@@ -0,0 +1,24 @@
+import { ExtractJwt, Strategy } from 'passport-jwt';
+import { PassportStrategy } from '@nestjs/passport';
+import { Injectable } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
+import { JwtUserPayload } from '../interfaces/jwt-user-payload.interface';
+import { UserService } from 'src/user/user.service';
+
+@Injectable()
+export class JwtStrategy extends PassportStrategy(Strategy) {
+  constructor(
+    configServie: ConfigService,
+    private readonly userService: UserService,
+  ) {
+    super({
+      jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
+      ignoreExpiration: false,
+      secretOrKey: configServie.get<string>('JWT_SECRET'),
+    });
+  }
+
+  async validate(payload: JwtUserPayload) {
+    return this.userService.findOne(payload.id);
+  }
+}
