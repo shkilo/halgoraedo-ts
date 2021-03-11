fa2849c716f0334c1dccd797d644cd8f94cf1919
shkillo <kyle.hwan.k@gmail.com>
2021-03-11 20:31:24 +0900

    feat: implement auth controller

---
+++ src/auth/auth.controller.ts
@@ -0,0 +1,19 @@
+import { Controller, Get, Req, UseGuards } from '@nestjs/common';
+import { AuthGuard } from '@nestjs/passport';
+import { AuthService } from './auth.service';
+
+@Controller('auth')
+export class AuthController {
+  constructor(private readonly authService: AuthService) {}
+
+  @Get('google')
+  @UseGuards(AuthGuard('google'))
+  async googleAuth() {}
+
+  @Get('google/callback')
+  @UseGuards(AuthGuard('google'))
+  googleAuthCallback(@Req() req) {
+    const token = this.authService.getJwtToken(req.user);
+    console.log(token);
+  }
+}
