c6f15cf53e1859d83753f9b1977d0a01a567fa79
shkillo <kyle.hwan.k@gmail.com>
2021-03-11 20:37:40 +0900

    feat: export user service

--- src/user/user.module.ts
+++ src/user/user.module.ts
@@ -8,5 +8,6 @@ import { UserService } from './user.service';
   imports: [SequelizeModule.forFeature([User])],
   providers: [UserService],
   controllers: [UserController],
+  exports: [UserService],
 })
 export class UserModule {}
