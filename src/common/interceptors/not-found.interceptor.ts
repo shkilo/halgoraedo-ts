1d311751ea0f808baa8cb38628a4df593eea3820
shkillo <kyle.hwan.k@gmail.com>
2021-03-14 22:01:55 +0900

    feat: change notFoundInterCeptor to catch error

--- src/common/interceptors/not-found.interceptor.ts
+++ src/common/interceptors/not-found.interceptor.ts
@@ -6,17 +6,20 @@ import {
   CallHandler,
 } from '@nestjs/common';
 import { Observable } from 'rxjs';
-import { tap } from 'rxjs/operators';
+import { catchError } from 'rxjs/operators';
+import { EntityNotFoundException } from '../exceptions/buisness.exception';
 
 @Injectable()
 export class NotFoundInterceptor implements NestInterceptor {
   constructor(private errorMessage: string) {}
 
-  intercept(context: ExecutionContext, stream$: CallHandler): Observable<any> {
-    return stream$.handle().pipe(
-      tap((data) => {
-        if (data === null) {
+  intercept(context: ExecutionContext, next: CallHandler): Observable<any> {
+    return next.handle().pipe(
+      catchError((error) => {
+        if (error instanceof EntityNotFoundException) {
           throw new NotFoundException(this.errorMessage);
+        } else {
+          throw error;
         }
       }),
     );
