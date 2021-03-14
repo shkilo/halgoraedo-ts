2952ffe5d026eba599d18993560ce8c11e30d221
shkillo <kyle.hwan.k@gmail.com>
2021-03-14 22:04:53 +0900

    feat: implement project controller
    - create, findAll, findOne, update, remove
    - use NotFoundInterceptor

--- src/project/project.controller.ts
+++ src/project/project.controller.ts
@@ -4,36 +4,62 @@ import {
   Delete,
   Get,
   Param,
+  Patch,
   Post,
+  Req,
+  UseGuards,
   UseInterceptors,
 } from '@nestjs/common';
+import { AuthGuard } from '@nestjs/passport';
+import { Request } from 'express';
 import { NotFoundInterceptor } from 'src/common/interceptors/not-found.interceptor';
 import { CreateProjectDto } from './dto/create-project.dto';
+import { UpdateProjectDto } from './dto/update-project.dto';
 import { Project } from './project.model';
 import { ProjectService } from './project.service';
 
 @Controller('project')
+@UseGuards(AuthGuard('jwt'))
 export class ProjectController {
   constructor(private readonly projectService: ProjectService) {}
 
-  // @Post()
-  // async create(@Body() createProjectDto: CreateProjectDto): Promise<Project> {
-  //   return await this.projectService.create(createProjectDto);
-  // }
+  @Post()
+  async create(
+    @Req() req: Request,
+    @Body() createProjectDto: CreateProjectDto,
+  ): Promise<Project> {
+    return await this.projectService.create(req.user, createProjectDto);
+  }
 
   @Get()
-  async findAll(): Promise<Project[]> {
-    return await this.projectService.findAll();
+  async findAll(@Req() req: Request): Promise<Project[]> {
+    return await this.projectService.findAll(req.user);
   }
 
   @Get(':id')
   @UseInterceptors(new NotFoundInterceptor('project not found'))
-  async findOne(@Param('id') id: string): Promise<Project> {
-    return await this.projectService.findOne(id);
+  async findOne(
+    @Req() req: Request,
+    @Param('id') id: string,
+  ): Promise<Project> {
+    return await this.projectService.findOne(req.user, parseInt(id, 10));
   }
 
-  // @Delete(':id')
-  // remove(@Param('id') id: string): Promise<void> {
-  //   return this.userService.remove(id);
-  // }
+  @Patch(':id')
+  async update(
+    @Req() req: Request,
+    @Param('id') id: string,
+    @Body() projectData: UpdateProjectDto,
+  ): Promise<Project> {
+    return await this.projectService.update(
+      req.user,
+      parseInt(id, 10),
+      projectData,
+    );
+  }
+
+  @Delete(':id')
+  remove(@Req() req: Request, @Param('id') id: string): Promise<void> {
+    return this.projectService.remove(req.user, id);
+  }
 }
