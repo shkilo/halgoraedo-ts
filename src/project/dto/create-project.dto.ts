ce603d340b8f84eb085d8bc0cddbcc569d7caa02
shkillo <kyle.hwan.k@gmail.com>
2021-03-15 22:21:58 +0900

    feat: add validator decorators to proejct dtos

--- src/project/dto/create-project.dto.ts
+++ src/project/dto/create-project.dto.ts
@@ -1,6 +1,18 @@
+import { IsBoolean, IsHexColor, IsOptional, IsString } from 'class-validator';
+
 export class CreateProjectDto {
+  @IsString()
   readonly title: string;
+
+  @IsOptional()
+  @IsBoolean()
   readonly isList?: boolean;
+
+  @IsOptional()
+  @IsHexColor()
   readonly color?: string;
+
+  @IsOptional()
+  @IsBoolean()
   readonly isFavorite?: boolean;
 }
