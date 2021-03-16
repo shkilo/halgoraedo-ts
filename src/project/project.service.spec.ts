9bc2ed8d4525843ea0733ac8b17df9be73cb91aa
shkillo <kyle.hwan.k@gmail.com>
2021-03-17 01:00:28 +0900

    test

--- src/project/project.service.spec.ts
+++ src/project/project.service.spec.ts
@@ -74,7 +74,7 @@ describe('ProjectService', () => {
 
   it('Find all project', async () => {
     const user = { id: 1 } as User;
-
+    user;
     expect(await service.findAll(user)).toEqual(foundProjects);
   });
 });
