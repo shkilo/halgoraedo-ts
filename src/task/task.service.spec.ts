749ebb5b39ed1b08c421630eaeb3645051a6b9bf
shkillo <kyle.hwan.k@gmail.com>
2021-04-08 15:34:40 +0900

    test: add unit test for task service

--- src/task/task.service.spec.ts
+++ src/task/task.service.spec.ts
@@ -1,18 +1,102 @@
+import { getModelToken } from '@nestjs/sequelize';
 import { Test, TestingModule } from '@nestjs/testing';
+import { EntityNotFoundException } from '../common/exceptions/buisness.exception';
+import { ProjectService } from '../project/project.service';
+import { Task } from './task.model';
 import { TaskService } from './task.service';
+
+const testTasks = [
+  {
+    id: 1,
+    position: 0,
+  },
+  {
+    id: 2,
+    position: 1,
+  },
+];
+const newTask = {
+  id: 3,
+  title: 'test',
+  position: 2,
+};
+const testSectionInstance = {
+  $get: jest.fn(() => testTasks),
+};
+const user = { id: 1 } as any;
 
 describe('TaskService', () => {
   let service: TaskService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [TaskService],
+      providers: [
+        TaskService,
+        {
+          provide: ProjectService,
+          useValue: {
+            findSection: jest.fn(() => testSectionInstance),
+          },
+        },
+        {
+          provide: getModelToken(Task),
+          useValue: {
+            create: jest.fn(() => newTask),
+            findAll: jest.fn(() => testTasks),
+            findOne: jest.fn((option) =>
+              option.where.id === testTasks[0].id ? testTasks[0] : undefined,
+            ),
+          },
+        },
+      ],
     }).compile();
-
     service = module.get<TaskService>(TaskService);
   });
 
   it('should be defined', () => {
     expect(service).toBeDefined();
+  });
+  it('Create a task', async () => {
+    const dto = {
+      title: 'test',
+      projectId: 1,
+      sectionId: 1,
+    } as any;
+    expect(await service.create(user, dto)).toEqual(newTask);
+  });
+  it('Find all tasks', async () => {
+    expect(await service.findAll(user)).toEqual(testTasks);
+  });
+  it('Find one task by id', async () => {
+    const id = testTasks[0].id;
+    const idNotExisting = 404;
+    // id existing
+    expect(await service.findOne(user, id)).toEqual(testTasks[0]);
+    // id not existing
+    await expect(service.findOne(user, idNotExisting)).rejects.toBeInstanceOf(
+      EntityNotFoundException,
+    );
+  });
+  it('Update a task', async () => {
+    const id = testTasks[0].id;
+    const dto = {
+      title: 'new title',
+    } as any;
+    const taskInstace: any = {
+      update() {
+        return testTasks[0];
+      },
+    };
+
+    jest.spyOn(service, 'findOne').mockImplementation(() => taskInstace);
+    expect(await service.update(user, id, dto)).toEqual(testTasks[0]);
+  });
+  it('Delete a task', async () => {
+    const taskInstace: any = {
+      destroy: jest.fn(() => undefined),
+    };
+
+    jest.spyOn(service, 'findOne').mockImplementation(() => taskInstace);
+    expect(await service.remove(user, testTasks[0].id)).toBeUndefined();
   });
 });
