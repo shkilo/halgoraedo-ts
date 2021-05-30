bb7c904319b7270952f59ccbf4eaf440cfc5acc8
shkillo <kyle.hwan.k@gmail.com>
2021-05-30 22:46:13 +0900

    test: update e2e test

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,10 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { globalRoutePrefix } from './common/constants';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.setGlobalPrefix(globalRoutePrefix);
   await app.listen(process.env.PORT);
 }
 bootstrap();
