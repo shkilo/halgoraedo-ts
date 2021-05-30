c4f1e3247050a3cad2875b773df39947325943bb
shkillo <kyle.hwan.k@gmail.com>
2021-05-30 22:42:24 +0900

    feat: update feature - login

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,19 +1,33 @@
-import { Controller, Get, Req, UseGuards } from '@nestjs/common';
+import { Controller, Get, Req, Res, UseGuards } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
 import { AuthGuard } from '@nestjs/passport';
-import { Request } from 'express';
+import { Request, Response } from 'express';
+import { userAgentIPhone } from '../common/constants';
 import { AuthService } from './auth.service';
 
 @Controller('auth')
 export class AuthController {
-  constructor(private readonly authService: AuthService) {}
+  constructor(
+    private readonly authService: AuthService,
+    private readonly configService: ConfigService,
+  ) {}
 
   @Get('google')
   @UseGuards(AuthGuard('google'))
-  async googleAuth() {}
+  googleAuth() {}
 
   @Get('google/callback')
   @UseGuards(AuthGuard('google'))
-  googleAuthCallback(@Req() req: Request) {
-    this.authService.getJwtToken(req.user);
+  googleAuthCallback(@Req() req: Request, @Res() res: Response) {
+    const clientUrl = this.configService.get<string>('CLIENT_URL');
+    const clientIOSUrl = this.configService.get<string>('CLIENT_IOS_URL');
+    const token = this.authService.getJwtToken(req.user);
+    console.log(token);
+    const userAgent = req.headers['user-agent'];
+
+    res.status(200).redirect(`${clientUrl}?token=${token}`);
+    // return userAgent.includes(userAgentIPhone)
+    //   ? res.redirect(`${clientIOSUrl}?token=${token}`)
+    //   : res.status(200).redirect(`${clientUrl}?token=${token}`);
   }
 }
